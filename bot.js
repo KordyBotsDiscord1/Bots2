6368364cb1617d6f21b2f20bc6d0993a5b6a6115
KordyBotsDiscord1 <47664485+KordyBotsDiscord1@users.noreply.github.com>
2019-02-15 15:11:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -344,15 +344,4 @@ client.on('message', message => {
     }
 });
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Music Bot By Last Codes`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers`);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
-
 client.login(process.env.BOT_TOKEN);
